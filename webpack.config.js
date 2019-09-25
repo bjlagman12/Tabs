93d59f9ae51e337a618c849a0a4672d864755908
Brian Lagman <brianlagman@Brians-MacBook-Pro.local>
2019-09-25 14:14:51 -0700

    Set up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,31 @@
-const path =  require('path');
+const path = require('path');
 
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '/src/index.js'),
+  output: {
+    path: path.join(__dirname, '/public/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /(.js$|.jsx$)/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.css']
+  }
+};
